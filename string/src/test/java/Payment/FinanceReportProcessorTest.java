f5d6775f05e4224bcafc28ec337e514b776ed998
fyfyhntf <elizavetaoshurkina@gmail.com>
2024-12-20 13:45:48 +0600

    Add files via upload

--- string/src/test/java/Payment/FinanceReportProcessorTest.java
+++ string/src/test/java/Payment/FinanceReportProcessorTest.java
@@ -23,7 +23,21 @@ public class FinanceReportProcessorTest {
         FinanceReport report2 = new  FinanceReport("Озонов Михаил Иванович", new Date(), payments1);
         assertEquals(report1, FinanceReportProcessor.nameBeginsWithLetter(report2, payments1, ' '));
     }
-
+    @Test
+    public void nameBeginsWithLetter2() {
+        List<Payment> payments1 = List.of(
+                new Payment("Егорова Яна Денисовна", 26, 6, 2023, 5600),
+                new Payment("Сотникова Анастасия Владимировна", 6, 11, 2023, 10000),
+                new Payment("Сотников Никита Сергеевич", 9, 9, 2023, 8900),
+                new Payment("Стёпин Филипп Алексеевич", 6, 1, 2023, 160)
+        );
+        List<Payment> payments = List.of(
+                new Payment("Егорова Яна Денисовна", 26, 6, 2023, 5600)
+        );
+        FinanceReport report1 = new  FinanceReport("Озонов Михаил Иванович", new Date(), payments);
+        FinanceReport report2 = new  FinanceReport("Озонов Михаил Иванович", new Date(), payments1);
+        assertEquals(report1, FinanceReportProcessor.nameBeginsWithLetter(report2, payments1, 'Е'));
+    }
 
     @Test
     public void valuePayments1() {
@@ -54,5 +68,22 @@ public class FinanceReportProcessorTest {
         assertThrows(IllegalArgumentException.class, () -> {
             FinanceReportProcessor.valuePayments(report, payments, -10);
         });
+    }
+    @Test
+    public void valuePayments3() {
+        List<Payment> payments = List.of(
+                new Payment("Егорова Яна Денисовна", 26, 6, 2023, 5600),
+                new Payment("Сотникова Анастасия Владимировна", 6, 11, 2023, 10000),
+                new Payment("Сотников Никита Сергеевич", 9, 9, 2023, 8900),
+                new Payment("Стёпин Филипп Алексеевич", 6, 1, 2023, 160)
+        );
+        Date date = new Date();
+        FinanceReport fr1 = new FinanceReport("Озонов Михаил Иванович", date, payments);
+        List<Payment> expectedPayments = List.of(
+                new Payment("Стёпин Филипп Алексеевич", 6, 1, 2023, 160)
+        );
+        FinanceReport expected = new FinanceReport("Озонов Михаил Иванович", date, expectedPayments);
+        FinanceReport filtered = FinanceReportProcessor.valuePayments(fr1, payments, 5000);
+        assertEquals(expected, filtered);
     }
 }
